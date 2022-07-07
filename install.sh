36351d1336bd259f783d4dcf83eb83b33347cd27
Cassio Lima <58428807+cassioprlm@users.noreply.github.com>
2022-07-07 16:31:42 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
- 
-# install golang latest version
- 
+
+# Checking if golang is installed
+
 if [ $(which go) ];then
-	echo -e "\n\033[0;32mGolang Already Installed\033[0m\n";
+	go_version=$(go version | awk -F'go' '{print $3}' |awk -F' ' '{print $1}' | cut -c3-4)
+	if [[ $go_version -ge 17 ]];then
+		echo -e "\n\033[0;32mGolang Already Installed\033[0m\n";
+	else
+		echo -e "\n\033[0;32mGolang version less than 1.17! Please install a version >= 1.17 \033[0m\n"
+		exit 1
+	fi
 else
-        echo -e "\n\033[1;31mGolang is not installed!\033[0m\n";
+        echo -e "\n\033[1;31mGolang is required! Please install golang >= 1.17\033[0m\n";
+	exit 1
 fi
 	#set the gopath environment variable
-	echo export PATH="$"PATH:/usr/local/go/bin >> ~/.bashrc
-	echo export GOPATH="$"HOME/go >> ~/.bashrc
-        echo export PATH="$"PATH:"$"GOROOT/bin:$GOPATH/bin >> ~/.bashrc
-	source ~/.bashrc
-
+	export GOPATH=$HOME/go
+        export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+	
+	
 golang_tools=(
         "https://github.com/tomnomnom/gron"
         "https://github.com/tomnomnom/waybackurls"
